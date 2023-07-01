3012943a9debfee49e87a40ac4b16653693b71a6
Abhishek Thakur <34036890+abhishekthakur0@users.noreply.github.com>
2023-07-01 09:24:53 +0530

    runtime issue fix

--- lib/Reading/View/reading_view.dart
+++ lib/Reading/View/reading_view.dart
@@ -8,7 +8,8 @@ import 'package:bookcompanion/Reading/Bloc/reading_bloc.dart';
 import 'package:bookcompanion/Utils/color_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:http/http.dart' as http;
 
 class ReadingView extends StatefulWidget {
   const ReadingView(
@@ -21,8 +22,8 @@ class ReadingView extends StatefulWidget {
 }
 
 class _ReadingViewState extends State<ReadingView> {
-  final Completer<PDFViewController> _pdfViewController =
-      Completer<PDFViewController>();
+  // final Completer<PDFViewController> _pdfViewController =
+  //     Completer<PDFViewController>();
   final StreamController<String> _pageCountController =
       StreamController<String>();
   int currentPage = 0;
@@ -36,143 +37,156 @@ class _ReadingViewState extends State<ReadingView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        systemOverlayStyle: SystemUiOverlayStyle(
-          statusBarColor: greyColor,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: greyColor,
+          ),
+          backgroundColor: greyColor,
+          title: InkWell(
+            onTap: () {
+              ReadingStatus readingStatus = ReadingStatus(
+                title: widget.bookData.title,
+                author: widget.bookData.author,
+                modifiedAt: DateTime.now().toUtc().toString(),
+                currentPage: currentPage,
+                id: widget.bookData.id,
+                totalPage: numberOfPages,
+                fileUrl: widget.bookData.fileUrl,
+              );
+              lastReadingBloc.publishLastReadingDetail(readingStatus);
+              readingBloc.updateReadingStatusOfBook(readingStatus);
+              myReadingBloc.fetchYourReadingBookStatus();
+              Navigator.pop(context);
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(
+                      0.2,
+                    ),
+                    offset: const Offset(0, 2),
+                    spreadRadius: 0,
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SizedBox(
+                    width: 178,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.bookData.title,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          widget.bookData.author,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(
+                      0.2,
+                    ),
+                    offset: const Offset(0, 2),
+                    spreadRadius: 0,
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  StreamBuilder(
+                      stream: _pageCountController.stream,
+                      builder: (context, AsyncSnapshot<String> snapshot) {
+                        return Text(
+                          snapshot.data ?? 'Loading',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 15,
+                          ),
+                        );
+                      }),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  const Icon(
+                    Icons.more_vert,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        backgroundColor: greyColor,
-        title: InkWell(
-          onTap: () {
-            ReadingStatus readingStatus = ReadingStatus(
-              title: widget.bookData.title,
-              author: widget.bookData.author,
-              modifiedAt: DateTime.now().toUtc().toString(),
-              currentPage: currentPage,
-              id: widget.bookData.id,
-              totalPage: numberOfPages,
-              fileUrl: widget.bookData.fileUrl,
+        body: FutureBuilder(
+          future: http
+              .get(Uri.parse(widget.bookData.fileUrl))
+              .then((value) => value.bodyBytes),
+          builder: (context, AsyncSnapshot<Uint8List> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return PDFView(
+              pdfData: snapshot.data!,
+              enableSwipe: true,
+              swipeHorizontal: true,
+              autoSpacing: false,
+              pageFling: false,
+              defaultPage: widget.initialPage,
+              onPageChanged: (int? current, int? total) {
+                currentPage = current ?? 0;
+                numberOfPages = total ?? 0;
+                _pageCountController.add('${current! + 1} - $total');
+              },
+              onViewCreated: (PDFViewController pdfViewController) async {
+                // _pdfViewController.complete(pdfViewController);
+                currentPage = await pdfViewController.getCurrentPage() ?? 0;
+                final int? pageCount = await pdfViewController.getPageCount();
+                _pageCountController.add('${currentPage + 1} - $pageCount');
+              },
             );
-            lastReadingBloc.publishLastReadingDetail(readingStatus);
-            readingBloc.updateReadingStatusOfBook(readingStatus);
-            myReadingBloc.fetchYourReadingBookStatus();
-            Navigator.pop(context);
           },
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(
-                    0.2,
-                  ),
-                  offset: const Offset(0, 2),
-                  spreadRadius: 0,
-                ),
-              ],
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Icon(
-                  Icons.arrow_back,
-                  color: Colors.black,
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                SizedBox(
-                  width: 180,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.bookData.title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                          fontSize: 16,
-                        ),
-                      ),
-                      Text(
-                        widget.bookData.author,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-        actions: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(
-                    0.2,
-                  ),
-                  offset: const Offset(0, 2),
-                  spreadRadius: 0,
-                ),
-              ],
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                StreamBuilder(
-                    stream: _pageCountController.stream,
-                    builder: (context, AsyncSnapshot<String> snapshot) {
-                      return Text(
-                        snapshot.data ?? 'Loading',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                          fontSize: 15,
-                        ),
-                      );
-                    }),
-                const SizedBox(
-                  width: 10,
-                ),
-                const Icon(
-                  Icons.more_vert,
-                  color: Colors.black,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-      body: PDF(
-        enableSwipe: true,
-        swipeHorizontal: true,
-        autoSpacing: false,
-        pageFling: false,
-        defaultPage: widget.initialPage,
-        onPageChanged: (int? current, int? total) {
-          currentPage = current ?? 0;
-          numberOfPages = total ?? 0;
-          _pageCountController.add('${current! + 1} - $total');
-        },
-        onViewCreated: (PDFViewController pdfViewController) async {
-          _pdfViewController.complete(pdfViewController);
-          currentPage = await pdfViewController.getCurrentPage() ?? 0;
-          final int? pageCount = await pdfViewController.getPageCount();
-          _pageCountController.add('${currentPage + 1} - $pageCount');
-        },
-      ).fromUrl(widget.bookData.fileUrl),
-    );
+        ));
   }
 }
